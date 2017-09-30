895d2302795910bb5767adec1810429bd95187ed
mmuellerstoffels <marc.mueller.stoffels@gmail.com>
2017-09-30 07:33:32 +0200

    Edits of tags in wtgDescriptor.xml; addition of wtgPowerCurveAssembler.py stub.

---
+++ wtgPowerCurveAssembler.py
@@ -0,0 +1,10 @@
+# Project: GBS Tool
+# Author: Dr. Marc Mueller-Stoffels, marc.mueller.stoffels@gmail.com
+# Date: September 30, 2017 -->
+# License: MIT License (see LICENSE file of this package for more information
+
+# This function will eventually return a functional estimate for the power curve of a wind turbine based on data provided
+# in 'wtgDescriptor.xml'.
+# Currently this is only a place holder since the script was mentioned in documentation.
+
+#TODO: Define and implement power curve assembler.
