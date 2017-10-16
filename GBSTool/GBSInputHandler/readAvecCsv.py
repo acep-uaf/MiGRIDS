fdee8efb2a719d5d70e288d59ac598cafcccbbf8
Marc Mueller-Stoffels <marc@denamics.com>
2017-10-16 16:29:06 +0200

    Added comments in readAvecCsv.py - mostly suggestions.

--- GBSTool/GBSInputHandler/readAvecCsv.py
+++ GBSTool/GBSInputHandler/readAvecCsv.py
@@ -1,6 +1,11 @@
+# TODO: File header here
+# TODO: Use more comments throughout ;-)
+# TODO: Bundle all `import' statements at the top
+
 print('Hello World')
 
 # cd to the Resources dir to get input files
+# TODO: Set this up such that it can easily setup with an interactive interface later (either command line or GUI)
 import os
 print(os.getcwd())
 dir = os.path.dirname(__file__)
@@ -8,7 +13,9 @@ newdir = os.path.join(dir,'..\..\InputData\Chevak')
 os.chdir(newdir)
 import os
 print(os.getcwd())
+
 # load csv file
+# TODO: While the import ... with ... definitely works this way, this also hard codes the file you are opening, which is not desirable.
 import csv
 with open('ChevakDispatch201612.csv','r') as csvfile:
     reader = csv.reader(csvfile,delimiter = ',')
@@ -18,6 +25,7 @@ with open('ChevakDispatch201612.csv','r') as csvfile:
 
 print(data[3])
 
+# TODO: yupp, pandas should do all the tricks we need it to do...
 import pandas as pd
 df = pd.read_csv('ChevakDispatch201612.csv')
 
@@ -26,6 +34,7 @@ x=np.array(df)
 x
 y=x.astype(np.float) # cannot convert string to float
 
+# TODO: have you already found a good cheat sheet for regular expressions? Let me know if you need one.
 import re
 non_decimal = re.compile(r'[^\d.]+')
 non_decimal.sub('', '12.345i5ii3')
