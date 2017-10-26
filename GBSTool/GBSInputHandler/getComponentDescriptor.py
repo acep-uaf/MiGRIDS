3c4afc25813ada9babc10b765d5224a25e5448a1
jbvandermeer <jbvandermeer@alaska.edu>
2017-10-25 16:52:39 -0800

    started work on getComponentDescriptor

--- GBSTool/GBSInputHandler/getComponentDescriptor.py
+++ GBSTool/GBSInputHandler/getComponentDescriptor.py
@@ -12,6 +12,19 @@ def getComponentDescriptor(varNames):
     from netCDF4 import Dataset
     ncfile = Dataset('test.nc', 'w', format='NETCDF4')
 
+    import os
+    os.chdir('C:\\Users\jbvandermeer\Documents\ACEP\GBS\GBSTools1\InputData\Chevak')
+    from bs4 import BeautifulSoup
+    infile_child = open("gen1Descriptor.xml", "r")
+    contents_child = infile_child.read()
+    infile_interface = open('gen1DescriptorInterface.xml','r')
+    contents_interface = infile_interface.read()
+
+
+    soup = BeautifulSoup(contents_child, 'xml')
+    titles = soup.find_all('title')
+    for title in titles:
+    print(title.get_text())
 
     return ncfile
 
