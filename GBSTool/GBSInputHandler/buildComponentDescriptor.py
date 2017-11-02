3c509f6c1516705474299ce7aabcd386600b20db
jbvandermeer <jbvandermeer@alaska.edu>
2017-11-01 17:16:29 -0800

    trying to get buildComponentDescriptor to read accept input component names, find the component descriptors, and combine with all parents into one file.

--- GBSTool/GBSInputHandler/buildComponentDescriptor.py
+++ GBSTool/GBSInputHandler/buildComponentDescriptor.py
@@ -6,6 +6,9 @@
 # this accepts a list of component names with acceptable format and generates empty component descriptor files for them
 # and saves them in the input directory specified. These files will then be updated each time the user updates the information
 def buildComponentDescriptor(componentNames,saveDir):
+    # componentNames is a list of all components to be included in the simulation
+    # saveDir is where the generated component descriptor files will be saved
+
     # General Imports
     from bs4 import BeautifulSoup
     import os
@@ -22,24 +25,39 @@ def buildComponentDescriptor(componentNames,saveDir):
         try:
             #ind = varnames.index(componentNames[i].lower())
             #ind = [varnames.index(i) for i in varnames if componentNames[i].lower() in i ]
-            ind = [i for i, s in enumerate(varnames) if componentNames[i].lower() in s] # get the index of varnmaes that matches the input componentName
+            ind = [j for j, s in enumerate(varnames) if s in componentNames[i].lower()]            # get the index of varnmaes that matches the input componentName
 
         except:
             print('Component name '+componentNames[i]+' is not valid.')
             print('Please use one of the following (case insensitive) as a prefix: ')
             print('%s' % ', '.join(map(str, varnames)))
             break
+        else:
+            # read the xml file
+            fileName = varnames[ind[0]]+'Descriptor.xml' # get filename
+            infile_child = open(fileName, "r") # open
+            contents_child = infile_child.read()
+            infile_child.close()
+            soup = BeautifulSoup(contents_child, 'xml') # turn into soup
+            parent = soup.childOf.string.lower() # find the anme of parent. if 'self', no parent file
+
+            while parent != 'self': # continue to iterate if there are parents
+                fileName = parent + '.xml'
+                infile_child = open(fileName, "r")
+                contents_child = infile_child.read()
+                infile_child.close()
+                soup2 = BeautifulSoup(contents_child, 'xml')
+                # add new elements from soup2 into soup
+                for element in soup2.body:
+                    soup.body.append(element)
+                # find parent. if 'self' then no parent
+                parent = soup2.childOf.string
+
+            # write combined xml file
+            saveName = componentNames[i]+'Descriptor.xml'
+            f = open(saveName, "w")
+            f.write(soup.prettify())
+            f.close()
 
 
-
-        fileName = varnames(ind)+'ComponentDescriptor.xml'
-        infile_child = open(fileName, "r")
-        contents_child = infile_child.read()
-        infile_interface = open(fileName, 'r')
-        contents_interface = infile_interface.read()
-
-        soup = BeautifulSoup(contents_child, 'xml')
-        titles = soup.find_all('title')
-        for title in titles:
-            print(title.get_text())
-
+buildComponentDescriptor(['gen1','gen2','wtg1'],'C:\\Users\jbvandermeer\Documents\ACEP\GBS\GBSTools1\GBSProjects\Chevak\InputData\Components')
--- GBSTool/GBSInputHandler/test.py
+++ GBSTool/GBSInputHandler/test.py
@@ -11,3 +11,9 @@ ncData = convertDataframeToNetcdf(x_df,'test.nc',['kW','kvar','kPA'])
 print(ncData.variables)
 
 
+# get children
+children = soup.findChildren()  # get all children
+for i in range(len(children)):
+    name = children[i].name
+    attributes = children[i].attrs
+    grandkids = children[i].findChildren
