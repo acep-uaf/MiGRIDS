f4ba91f385fa4e6fd548508749cc1b4d96e5d109
Tawna C Morgan <tcmorgan2@alaska.edu>
2017-12-19 18:53:34 -0900

    added data match and replace functionality

--- GBSTool/GBSInputHandler/fixBadData.py
+++ GBSTool/GBSInputHandler/fixBadData.py
@@ -3,7 +3,7 @@
 # Date: October 24, 2017
 # License: MIT License (see LICENSE file of this package for more information)
 
-import numpy as np
+
 import pandas as pd
 import xml.etree.ElementTree as ET
 import os
@@ -14,6 +14,7 @@ DATETIME = 'datetime'
 SETUPXML = 'Setup.xml'
 DESCXML = 'Descriptor.xml'
 COMPSUFFIX = 'output'
+SEARCHSTART = 70
 
 # reads dataframe of data input compares values to descriptor xmls and returns DataClass object
 #assumes df column headings match header values in setup.xml
@@ -22,18 +23,22 @@ COMPSUFFIX = 'output'
 def fixBadData(df,setupDir,projectName):
     #TODO check dataframe sorting order.
     #create DataClass object to store raw, fixed, and summery output
-    MyData = DataClass(df)   
+    MyData = DataClass(df) 
+    
     #run through data checks for each component
     for component in getComponents(setupDir,''.join([projectName, SETUPXML])):
+        component = '_'.join([component, COMPSUFFIX])
         try:
             #find repeated values first
-            identifyInline(component,MyData)
-            dataMatchReplace(MyData.fixed, component)
+            identifyInline(component,MyData)           
+            dataMatchReplace(MyData.fixed, component) 
             checkMinMaxPower(component, MyData, setupDir) 
-            linearFix(MyData.fixed[ MyData.fixed['_'.join([component,COMPSUFFIX])].isnull()].index.tolist(),
-                                             MyData.fixed,'_'.join([component,COMPSUFFIX])) 
+            
+            linearFix(MyData.fixed[MyData.fixed[component].isnull()].index.tolist(),
+                                             MyData.fixed,component) 
+            print(MyData.fixed)
         except KeyError:
-            print('no column named %s' %'_'.join([component,COMPSUFFIX]))
+            print('no column named %s' %component)
         #TODO add confirmation before replacing data values
        
     MyData.summerize()
@@ -44,47 +49,79 @@ def fixBadData(df,setupDir,projectName):
 def identifyInline(component, data):
 
     b = pd.DataFrame({
-            'a':data.fixed['_'.join([component,COMPSUFFIX])].diff(1).abs(),
-            'b':data.fixed['_'.join([component,COMPSUFFIX])].diff(-1).abs()
+            'a':data.fixed[component].diff(1).abs(),
+            'b':data.fixed[component].diff(-1).abs()
             }).min(1)==0
     
     #inline values are set to NaN so they can be replaced
-    data.fixed['_'.join([component,COMPSUFFIX])] = data.fixed['_'.join([component,COMPSUFFIX])].mask(b)
-    bad_dict_add('_'.join([component,COMPSUFFIX]),
+    data.fixed[component] = data.fixed[component].mask(b)
+    badDictAdd(component,
                  data.baddata,'2.Inline values',
-    data.fixed['_'.join([component,COMPSUFFIX])][pd.isnull(data.fixed['_'.join([component,COMPSUFFIX])])].index.tolist() )
+    data.fixed[component][pd.isnull(data.fixed[component])].index.tolist() )
  
-def bad_dict_add(component, current_dict, error_msg, index_list):
+def badDictAdd(component, current_dict, error_msg, index_list):
     #if the component exists add the new error message, otherwise start the compnent
     try:
         current_dict[component][error_msg]=index_list
     except KeyError:
         current_dict[component]= {error_msg:index_list}
         
-#DataClass -> DataClass
+#DataFrame, String -> Dictionary
+#returns dictionary of start and end times for blocks of null values for a component on a dataframe
+def getTimespans(df, component):
+    d = df.astype(str).groupby(component).rank(ascending = True)[DATETIME] 
+    starts = df[DATETIME][d == 1 & df[component].isnull()].index.tolist()
+    d = df.astype(str).groupby(component).rank(ascending = False)[DATETIME] 
+    ends = df[DATETIME][d == 1 & df[component].isnull()].index.tolist()
+    
+    return {'starts':starts,'ends':ends}
+    
+#Dataframe -> Dataframe
 #replaces individual bad values with linear estimate from surrounding values
-
 def dataMatchReplace(df,component):
-    #TODO write function
-    #place holder replaces inline values with 300
-    df['_'.join([component,COMPSUFFIX])][pd.isnull(df['_'.join([component,COMPSUFFIX])])]=300
+    
+    #get dictionary of start and end times for blocks of missing data
+    time_dict = getTimespans(df, component)
+    
+    for block in range(0,len(time_dict['starts'])):
+        start_index = df[df[DATETIME] == time_dict['starts'][block]].index.tolist()[0]
+        time_span = time_dict['ends'][block]-time_dict['starts'][block]
+        time_span_in_rows = len(df[df[DATETIME] < time_dict['ends'][block]]) - len(df[df[DATETIME] < time_dict['starts'][block]])
+        #search start is 1.2 year from missing data
+        search_start = time_dict['starts'][block] - SEARCHSTART
+        if search_start < 0:
+            search_start = time_span
+        original_block =  df[component][(df[DATETIME] > time_dict['starts'][block] - 0.5 * time_span) & (df[DATETIME] <= time_dict['ends'][block] + 0.5 * time_span)]
+        #now we look for a block of time with similar distribution
+        ps= df[component][df[DATETIME] > search_start].rolling(time_span_in_rows).apply(lambda x: doMatch(x,original_block))
+        #first_valid is the index of the last value in the subset of data that has a matching distribution to our missing data
+        first_valid = (ps[ps > 0.10]).first_valid_index()
+        if first_valid != None:
+            
+            dataReplace(df, component, first_valid,start_index,time_span_in_rows)
+        else:
+            index_list = df[start_index:(start_index + time_span_in_rows)].index.tolist()
+            linearFix(index_list,df,component)
     return df
-           
+
+#Dataframe, integer, integer -> dataframe
+def dataReplace(df,component, replacement_last_i, start_i,replacement_length):
+    df.loc[start_i:start_i + replacement_length,component] = df[(replacement_last_i - replacement_length):(replacement_last_i + 1)][component].values
+    return df
+
+def doMatch(x,y):   
+    return stats.ks_2samp(x,y).pvalue
+     
 def linearFix(index_list, df,component):
      
      for i in index_list: 
-         print (i)
-         df.set_value(i, component,linearEstimate(df[DATETIME][getIndex(df,i,component)],
-                                                  df[component][getIndex(df,i,component)],
-                                                  df[DATETIME][i]))
-         print(df)
-         return df
+         df.loc[i, component]= linearEstimate(df[DATETIME][getIndex(df,i,component)],df[component][getIndex(df,i,component)], df[DATETIME][i])
+         
+     return df
                     
 #numeric array, numeric array, Integer -> float  
 # X is array of x values (time), y is array of y values (power), t is x value to predict for. 
-def linearEstimate(x,y,t):
-    print(x)
-    print(y)
+def linearEstimate(x,y,t):    
     k = stats.linregress(x,y)
     return  k.slope *t + k.intercept  
 
@@ -113,7 +150,7 @@ def getPrevious(i,component):
 #returns a dictionary of bad values for a given variable
 def checkMinMaxPower(component, data, setupDir):
     #look up possible min max
-    descriptorxmlpath = os.path.join(setupDir,'Components',''.join([component,DESCXML]))
+    descriptorxmlpath = os.path.join(setupDir,'Components',''.join([component[0:4],DESCXML]))
     try:
         descriptorxml = ET.parse(descriptorxmlpath)
             
@@ -121,15 +158,14 @@ def checkMinMaxPower(component, data, setupDir):
         max_power = getValue(descriptorxml,"POutMaxPa")
         min_power = 0
         try:
-            over = data.fixed['_'.join([component, COMPSUFFIX])] > max_power 
-            under = data.fixed['_'.join([component, COMPSUFFIX])] < min_power
-            data.fixed['_'.join([component, COMPSUFFIX])] = data.fixed['_'.join([component, COMPSUFFIX])].mask((over | under))
-            bad_dict_add('_'.join([component,COMPSUFFIX]),
-                     data.baddata,'1.Exceeds Min/Max',
-                     data.fixed['_'.join([component,COMPSUFFIX])][pd.isnull(data.fixed['_'.join([component,COMPSUFFIX])])].index.tolist() )
+            over = data.fixed[component] > max_power 
+            under = data.fixed[component] < min_power
+            data.fixed[component] = data.fixed[component].mask((over | under))
+            badDictAdd(component,data.baddata,'1.Exceeds Min/Max',
+                     data.fixed[component][pd.isnull(data.fixed[component])].index.tolist() )
   
         except KeyError:
-            print ('_'.join([component, COMPSUFFIX]) + " was not found in the dataframe")
+            print ( "%s was not found in the dataframe" %component)
     except FileNotFoundError:
         print ('Descriptor xml for %s not found' %component)
     return 
