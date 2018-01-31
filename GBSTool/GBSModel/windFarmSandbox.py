2ce488f89e11e4ce04efd385d6c2136cddbc50d6
jbvandermeer <jbvandermeer@alaska.edu>
2018-01-30 18:06:04 -0900

    Fixed wind power curve in wtg1Descriptor

--- GBSTool/GBSModel/windFarmSandbox.py
+++ GBSTool/GBSModel/windFarmSandbox.py
@@ -11,7 +11,7 @@ timeStep = 1
 wtgDescriptor = ['C:\\Users\jbvandermeer\Documents\ACEP\GBS\GBSTools\GBSProjects\Chevak\InputData\Components\wtg1Descriptor.xml']*3
 windSpeed = []
 for i in range(10000):
-    windSpeed.append(randint(0,600))
+    windSpeed.append(randint(0,30))
 
 WF1 = Windfarm(wtgIDs, wtgP, wtgQ, windSpeed, wtgStates, timeStep, wtgDescriptor)
 
@@ -19,7 +19,7 @@ wtg0P = []
 wtg1P = []
 wtg2P = []
 for i in range(10000):
-    WF1.wtgDispatch(400,0)
+    WF1.wtgDispatch(70,0)
     wtg0P.append(WF1.windTurbines[0].wtgP)
     wtg1P.append(WF1.windTurbines[1].wtgP)
     wtg2P.append(WF1.windTurbines[2].wtgP)
