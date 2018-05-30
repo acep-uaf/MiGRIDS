8f381c42e85ccd90eef572c6a30d2a456a26b653
Tawna Morgan <tawnamorgan@PSIMacPro.local>
2018-05-30 14:50:35 -0800

    call UI moved to controller

---
+++ GBSTool/GBSController/callUIForm.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+import sys
+from PyQt5 import QtWidgets,QtSql
+
+from GBSUserInterface.FormMain import MainForm
+from GBSUserInterface.ProjectSQLiteHandler import ProjectSQLiteHandler
+
+def callUIForm():
+
+    sys._excepthook = sys.excepthook
+    def my_exception_hook(exctype, value, traceback):
+        # Print the error and traceback
+        print(exctype, value, traceback)
+        # Call the normal Exception hook after
+        sys._excepthook(exctype, value, traceback)
+        sys.exit(1)
+    
+    # Set the exception hook to our wrapping function
+    sys.excepthook = my_exception_hook
+
+    app = QtWidgets.QApplication(sys.argv)
+    #start with an empty default database called project_manager
+    
+    handler = ProjectSQLiteHandler()
+    handler.makeDatabase()
+
+    #make the database available to the form models
+    db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+    db.setDatabaseName('project_manager')
+
+    #launch the main form which contains subforms
+    s = MainForm()
+    try:
+        sys.exit(app.exec_())
+    except:
+        print('exiting')
+
