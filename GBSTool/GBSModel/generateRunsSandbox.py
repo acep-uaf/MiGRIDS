0212e1d8c1c0749dfb26ebf068935b71366396f0
jbvandermeer <jbvandermeer@alaska.edu>
2018-08-20 18:44:05 -0800

    sanboxes modified

--- GBSTool/GBSModel/generateRunsSandbox.py
+++ GBSTool/GBSModel/generateRunsSandbox.py
@@ -1,5 +1,5 @@
 from generateRuns import generateRuns
 import os
 here = os.path.dirname(os.path.realpath(__file__))
-generateRuns(os.path.join(here,"../../GBSProjects/StMary/OutputData/Set16"))
+generateRuns(os.path.join(here,"../../GBSProjects/StMary/OutputData/Set18"))
 #generateRuns(here + '/../../GBSProjects/ControlProject1/OutputData/SetBaseLine')
--- GBSTool/GBSModel/runSimulationSandbox.py
+++ GBSTool/GBSModel/runSimulationSandbox.py
@@ -4,4 +4,6 @@ import sys
 import os
 here = os.path.dirname(os.path.realpath(__file__))
 runSimulation(here + '/../../GBSProjects/StMary/OutputData/Set16')
+runSimulation(here + '/../../GBSProjects/StMary/OutputData/Set17')
+runSimulation(here + '/../../GBSProjects/StMary/OutputData/Set18')
 #runSimulation(here + '/../../GBSProjects/ControlProject1/OutputData/SetBaseLine')
