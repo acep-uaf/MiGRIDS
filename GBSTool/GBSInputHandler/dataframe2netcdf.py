82ae3403c20be0c8c3b050647ebf5bd59c062ab6
Tawna Morgan <tcmorgan2@alaska.edu>
2018-10-03 10:13:23 -0800

    netcdf file tracking added to input handler and user interface

--- GBSTool/GBSInputHandler/dataframe2netcdf.py
+++ GBSTool/GBSInputHandler/dataframe2netcdf.py
@@ -26,31 +26,35 @@ def dataframe2netcdf(df,components,saveLocation=''):
         root.attributes('-topmost', 1)
         saveLocation = filedialog.askdirectory()
     os.chdir(saveLocation)
-
+    netCDFList = []
     # create a netCDF file for each data column of the data frame containing the timestamps and the data
     for component in components.keys():
         # get header name from dataframe
-
-        column = [c for c,x in enumerate(df.columns.values) if x == component]
-        if len(column) >0:
-            column = column[0]
-        ncName = component + '.nc'
-        rootgrp = Dataset(ncName, 'w', format='NETCDF4') # create netCDF object
-        rootgrp.createDimension('time', None)  # create dimension for all called time
-        # create the time variable
-        rootgrp.createVariable('time', df.dtypes[column], 'time')  # create a var using the varnames
-        rootgrp.variables['time'][:] = np.array(df.index.astype(np.int64)//10**9)  # fill with values
-        # create the value variable
-        rootgrp.createVariable('value', df.dtypes[column], 'time')  # create a var using the varnames
-        rootgrp.variables['value'][:] = np.array(df[component])  # fill with values
-        # assign attributes
-        rootgrp.variables['time'].units = 'seconds'  # set unit attribute
-        rootgrp.variables['value'].units = components[component]['units'] # set unit attribute
-        rootgrp.variables['value'].Scale = components[component]['scale'] # set unit attribute
-        rootgrp.variables['value'].offset = components[component]['offset']  # set unit attribute
-        # close file
-        rootgrp.close()
-
+        try:
+            column = [c for c,x in enumerate(df.columns.values) if x == component]
+            if len(column) >0:
+                column = column[0]
+            ncName = component + '.nc'
+            rootgrp = Dataset(ncName, 'w', format='NETCDF4') # create netCDF object
+            rootgrp.createDimension('time', None)  # create dimension for all called time
+            # create the time variable
+            rootgrp.createVariable('time', df.dtypes[column], 'time')  # create a var using the varnames
+            rootgrp.variables['time'][:] = np.array(df.index.astype(np.int64)//10**9)  # fill with values
+            # create the value variable
+            rootgrp.createVariable('value', df.dtypes[column], 'time')  # create a var using the varnames
+            rootgrp.variables['value'][:] = np.array(df[component])  # fill with values
+            # assign attributes
+            rootgrp.variables['time'].units = 'seconds'  # set unit attribute
+            rootgrp.variables['value'].units = components[component]['units'] # set unit attribute
+            rootgrp.variables['value'].Scale = components[component]['scale'] # set unit attribute
+            rootgrp.variables['value'].offset = components[component]['offset']  # set unit attribute
+            # close file
+            rootgrp.close()
+            netCDFList.append(component)
+        except:
+            #if a netcdf file wasn't created successfully, move on to the next one
+            continue
     # return to the starting directory
     os.chdir(here)
+    return netCDFList
 
